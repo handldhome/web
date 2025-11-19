6966264a06213d4706a249607dbeec9e9e764808
handldhome <brad@handldhome.com>
2025-11-18 21:40:53 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import HowItWorks from "@/components/HowItWorks";
-import Bundld from "@/components/Bundld";
-import About from "@/components/About";
-import FinalCTA from "@/components/FinalCTA";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import HowItWorks from "../components/HowItWorks";
+import Bundld from "../components/Bundld";
+import About from "../components/About";
+import FinalCTA from "../components/FinalCTA";
 
 export default function Page() {
   return (
